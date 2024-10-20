20c4e39cdc3240ef848a6f007611b3c9949aded9
seiya <20365512+seiyab@users.noreply.github.com>
2024-10-20 13:13:35 +0900

    minor tweak

--- entry.sh
+++ entry.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
-chown -R user:user /home/user/projects/*
+chown -R user:user /home/user/projects
+chown -R user:user /home/user/.history
 chown -R user:user /home/user/.vscode-server
 service ssh start -D
