9c81323a1d1d9124b9c846f8eef5b9d0c2f9ec2e
seiya <20365512+seiyab@users.noreply.github.com>
2025-11-15 22:33:38 +0900

    omlet

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -1,1 +1,3 @@
 git config --global url."git@github.com:".pushInsteadOf "https://github.com/"
+
+echo 'github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl' >> '~/.ssh/known_hosts'
